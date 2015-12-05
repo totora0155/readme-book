4d0cedf54b45e8209d125471d4bbf3f872d16e13
nju33 <jun.sasaki@mvrck.co.jp>
2015-12-06 05:15:05 +0900

    Add .stylelintrc

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,39 @@
 const gulp = require('gulp'),
-      eslint = require('gulp-eslint');
+      eslint = require('gulp-eslint'),
+      postcss = require('gulp-postcss'),
+      cssvars = require('postcss-cson-cssvars'),
+      namespace = require('postcss-namespace'),
+      preref = require('postcss-preref'),
+      stylelint = require('stylelint')
+      reporter = require('postcss-reporter')
+      cssnano = require('cssnano');
 
-files = [
+const eslintFiles = [
   '**/*.js',
   '!node_modules/**/*.js'
 ];
 
 gulp.task('eslint', () => {
-  gulp.src(files).pipe(eslint())
+  gulp.src(eslintFiles).pipe(eslint())
     .pipe(eslint.format());
-    // .pipe(eslint.failAfterError());
 });
 
-gulp.task('lint', ['eslint'], (e) => {
+gulp.task('postcss', () => {
+  gulp.src('./src/*.css')
+    .pipe(postcss([
+      cssvars,
+      namespace,
+      preref,
+      stylelint,
+      reporter({clearMessages: true}),
+      cssnano
+    ]))
+    .pipe(gulp.dest('./templates'));
+});
+
+gulp.task('default', ['eslint', 'postcss'], (e) => {
   if (e.type !== 'deleted') {
-    gulp.watch(files, ['eslint']);
+    gulp.watch('./src/*.css', ['postcss']);
+    gulp.watch(esLintfiles, ['eslint']);
   }
 });
