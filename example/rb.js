2361b0ba3cb70e6e6056dc3fe0257b9a39b3c7e2
nju33 <jun.sasaki@mvrck.co.jp>
2015-12-19 05:44:38 +0900

    Add example

---
+++ example/rb.js
@@ -0,0 +1,9 @@
+const readmeBook = require('..');
+
+readmeBook({
+  urls: [
+    'https://github.com/totora0155/readme-book/blob/master/samples/a.md',
+    'https://github.com/totora0155/readme-book/blob/master/samples/b.md',
+    'https://github.com/totora0155/readme-book/blob/master/samples/c.md'
+  ]
+});
